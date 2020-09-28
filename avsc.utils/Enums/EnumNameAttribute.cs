2098a982a371edc1b5b3111d74206b85f264cad5
Vladislav Setchin <vlad@setchinfc.com.au>
2020-09-28 11:22:35 +1000

    Added Enum and Enum extensions

---
+++ avsc.utils/Enums/EnumNameAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace AVSC.Utils.Enums
+{
+    public class EnumName : Attribute
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ avsc.utils/Extensions/EnumExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AVSC.Utils.Enums;
+
+namespace AVSC.Utils.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T GetEnumAttribute<T>(this Enum val) where T : System.Attribute
+        {
+            var memColl = val.GetType().GetMember(val.ToString());
+
+            if (memColl.Length > 0)
+            {
+                var attributes = memColl[0].GetCustomAttributes(typeof(T), false);
+
+                if (attributes.Length > 0)
+                {
+                    return (T)attributes[0];
+                }
+            }
+
+            return default(T);
+        }
+
+        public static string EnumToString(this Enum enumValue)
+        {
+            string result = string.Empty;
+
+            var attribute = enumValue.GetEnumAttribute<EnumName>();
+
+            if(attribute == null)
+            {
+                result = enumValue.ToString();
+            }
+            else
+            {
+                result = attribute.Name;
+            }
+            
+            return result;
+        }
+    }
+}
